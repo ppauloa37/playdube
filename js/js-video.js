266487bd0e7fd04c06978ace542f4c3fb8367e71
Paulfox <ppauloa37@gmail.com>
2025-05-17 22:34:45 -0300

    Update js-video.js

--- js/js-video.js
+++ js/js-video.js
@@ -64,3 +64,60 @@
         button2.addEventListener("click", () => {
             numvide - 1;
         });
+    var container = document.getElementById("container");
+    var fullscreen = document.getElementById("fullscreen");
+    var content = document.getElementById("content");
+    var bars = document.getElementById("progress"),vbar = document.getElementById("volume"),vlt1 = document.getElementById("vlt1")
+    var vlt2 = document.getElementById("vlt2"),paused = document.getElementById("paused"),vbut = document.getElementById("vbut")
+
+    var primary = function() {
+        container.style.width = (window.innerWidth) + "px";
+        container.style.height = (window.innerHeight) + "px";
+        video.style.height = (window.innerHeight) + "px";
+        video.style.width = (window.innerWidth) + "px";
+        content.style.height = (window.innerHeight) + "px";
+        vlt1.style.left = (window.innerHeight + 32) + "px";
+        vlt2.style.left = (window.innerHeight + 1) + "px";
+        paused.style.left = (window.innerHeight - 56.5) + "px";
+        bars.style.left = (window.innerHeight - 200) + "px";
+        vbar.style.left = (window.innerHeight + 500) + "px";
+        fullscreen.style.left = (window.innerHeight - 30) + "px";
+        vbut.style.left = (window.innerHeight + 100) + "px";        
+    }
+        primary();
+    fullscreen.addEventListener("click", (e) => {
+        if (container.requestFullscreen) {
+        container.requestFullscreen();
+        video.style.width = (window.innerWidth) + "px";
+        video.style.height = (window.innerHeight) + "px";
+        container.style.width = (window.innerWidth) + "px";
+        container.style.height = (window.innerHeight) + "px";
+        vlt1.style.top = (window.innerHeight -20) + "px";
+        vlt2.style.top = (window.innerHeight - 20) + "px";
+        paused.style.top = (window.innerHeight - 20) + "px";
+        bars.style.top = (window.innerHeight - 25) + "px";
+        vbar.style.top = (window.innerHeight - 25) + "px";
+        vbar.style.width = "170px"
+        vbar.style.left = "1100px"
+        bars.style.width = "1000px"
+        fullscreen.style.top = (window.innerHeight - 20) + "px";
+        vbut.style.top = (window.innerHeight - 20) + "px";
+    }});
+    document.addEventListener("keydown", (e) => {
+        if (container.requestFullscreen) {
+        } else if (e.key === "Escape") {
+        video.style.width = "800px";
+        video.style.height = "450px";
+        e.preventDefault();
+        }
+    });
+    vbut.addEventListener("click", () => {
+        if (vbut.textContent === "🔊") {
+            vbar.style.display = "visible";
+            vbut.textContent = "❌";
+        } else {
+            vbar.style.display = "visible";
+            vbut.textContent = "🔊"
+        }
+    })
+
